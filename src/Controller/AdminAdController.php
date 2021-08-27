ee9be92661836f2da8d54857ef6494252293edaa
Tayeb Chakroun <tayebchakroune@gmail.com>
2021-08-27 03:30:30 +0100

    Admin control V1

---
+++ src/Controller/AdminAdController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Ad;
+use App\Form\AdType;
+use App\Repository\AdRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class AdminAdController extends AbstractController
+{
+    /**
+     * @Route("/admin/ads", name="admin_ads_index")
+     * @param AdRepository $repo
+     * 
+     * @return Response
+     */
+    public function index(AdRepository $repo): Response
+    {
+        return $this->render('admin/ad/index.html.twig', [
+            'ads' => $repo->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/admin/ads/{id}/edit", name="admin_ads_edit")
+     *
+     * @param Ad $ad
+     * @param Request $request
+     * @param EntityManagerInterface $manager
+     * @return Response
+     */
+    public function edit(Ad $ad, Request $request, EntityManagerInterface $manager): Response
+    {
+        $form = $this->createForm(AdType::class, $ad);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) 
+        {
+            $manager->persist($ad);
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                "L'annonce <strong>{$ad->getTitle()}</strong> a bien été enregistrée !"
+            );
+        }
+        
+        return $this->render('admin/ad/update.html.twig', [
+            'ad' => $ad,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/admin/ads/{id}/delete", name="admin_ads_delete")
+     * 
+     * @param Ad $ad
+     * @param EntityManagerInterface $manager
+     * @return Response
+     */
+    public function delete(Ad $ad, EntityManagerInterface $manager): Response
+    {
+        if(count($ad->getBookings()) > 0) {
+            
+            $this->addFlash(
+                'warning',
+                "Vous ne pouvez pas supprimer l'annonce <strong>{$ad->getTitle()}</strong> car elle possède déjà des réservations !"
+            );
+            
+        } else {
+            
+            $manager->remove($ad);
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                "L'annonce <strong>{$ad->getTitle()}</strong> a bien été supprimée !"
+            );
+        }
+        
+        return $this->redirectToRoute('admin_ads_index');
+    }
+}
---
+++ src/Form/AdminBookingType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Booking;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+
+class AdminBookingType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('startDate', DateType::class, [
+                'widget' => 'single_text'
+            ])
+            ->add('endDate', DateType::class, [
+                'widget' => 'single_text'
+            ])
+            ->add('comment')
+            ->add('createdAt')
+            ->add('amount')
+            ->add('booker')
+            ->add('ad')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Booking::class,
+        ]);
+    }
+}
