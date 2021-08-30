33634f2dbfbfc6243f420f1b97501ba6193c6c07
Tayeb Chakroun <tayebchakroune@gmail.com>
2021-08-30 20:36:30 +0200

    webpack config not finished

--- assets/app.js
+++ assets/app.js
@@ -6,8 +6,8 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import './styles/bootstrap.min.css';
-import './styles/app.min.css';
+//import './styles/bootstrap.min.css';
+//import './styles/app.min.css';
 
 // start the Stimulus application
-import './bootstrap';
+//import './bootstrap';
